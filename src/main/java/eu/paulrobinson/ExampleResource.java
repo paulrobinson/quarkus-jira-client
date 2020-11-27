c46ae01aadb5dffb58d07075ffceabbd27682e6d
Paul Robinson <paul.robinson@redhat.com>
2020-11-27 16:43:48 +0000

    Added JIRA client deps and code to connect the client

--- src/main/java/eu/paulrobinson/ExampleResource.java
+++ src/main/java/eu/paulrobinson/ExampleResource.java
@@ -5,12 +5,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Path("/resteasy/hello")
 public class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public String hello() throws URISyntaxException {
+        final JiraRestClient restClient = new AsynchronousJiraRestClientFactory().createWithBasicHttpAuthentication(new URI("https://issues.redhat.com"), "blah", "blah");
         return "hello";
     }
 }
